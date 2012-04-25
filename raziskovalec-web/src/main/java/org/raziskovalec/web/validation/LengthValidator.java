e3383e2410175fa8b8afbe9c8464fa4e278fabeb
Rene Svetina <rene.svetina@gmail.com>
2012-04-25 09:13:18 +0200

    Prestavil taglib

--- raziskovalec-web/src/main/java/org/raziskovalec/web/validation/LengthValidator.java
+++ raziskovalec-web/src/main/java/org/raziskovalec/web/validation/LengthValidator.java
@@ -1,18 +1,3 @@
-/**
- * Copyright 2012 Rene Svetina
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 /*
  * Copyright 2011 Rene Svetina.
  *
@@ -38,7 +23,7 @@ import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
 import org.raziskovalec.base.ObjectsUtil;
-import org.raziskovalec.web.jsf.Functions;
+import org.raziskovalec.jsf.Functions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -50,7 +35,7 @@ import com.sun.faces.util.MessageFactory;
  * @author Rene Svetina
  */
 public final class LengthValidator implements
-									Validator
+		Validator
 {
 	// =================================================================================================================
 	// Fields
@@ -80,9 +65,8 @@ public final class LengthValidator implements
 
 	/*
 	 * (non-Javadoc)
-	 * @see
-	 * javax.faces.validator.Validator#validate(javax.faces.context.FacesContext
-	 * , javax.faces.component.UIComponent, java.lang.Object)
+	 * @see javax.faces.validator.Validator#validate(javax.faces.context.FacesContext ,
+	 * javax.faces.component.UIComponent, java.lang.Object)
 	 */
 	@Override
 	public void validate(final FacesContext context, final UIComponent component, final Object value)
--- raziskovalec-web/src/main/java/org/raziskovalec/web/validation/RequiredValidator.java
+++ raziskovalec-web/src/main/java/org/raziskovalec/web/validation/RequiredValidator.java
@@ -1,18 +1,3 @@
-/**
- * Copyright 2012 Rene Svetina
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 /*
  * Copyright 2011 Rene Svetina.
  *
@@ -37,7 +22,7 @@ import javax.faces.context.FacesContext;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-import org.raziskovalec.web.jsf.Functions;
+import org.raziskovalec.jsf.Functions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,7 +34,7 @@ import com.sun.faces.util.MessageFactory;
  * @author Rene Svetina
  */
 public final class RequiredValidator implements
-									Validator
+		Validator
 {
 	// =================================================================================================================
 	// Fields
