28177eb1471e4fc36fafaba64303d5826d57de66
Rene Svetina <rene.svetina@gmail.com>
2012-04-26 12:16:22 +0200

    Testiranje pošiljanja HTMl emaila.

--- raziskovalec-web/src/main/java/org/raziskovalec/web/user/ResearcherAddBackingBean.java
+++ raziskovalec-web/src/main/java/org/raziskovalec/web/user/ResearcherAddBackingBean.java
@@ -15,15 +15,29 @@
  */
 package org.raziskovalec.web.user;
 
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.Map;
+
+import javax.faces.context.FacesContext;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Maps;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 
 /**
  * Researcher backing bean for adding new researchers.
@@ -38,6 +52,7 @@ public final class ResearcherAddBackingBean
 	private final Logger			logger	= LoggerFactory.getLogger(this.getClass());
 	private final ResearcherBean	researcherBean;
 	private final Session			session;
+	private final Configuration		freemarkerConfiguration;
 
 	// =================================================================================================================
 	// Constructors
@@ -49,10 +64,13 @@ public final class ResearcherAddBackingBean
 	 * @param researcherBean
 	 *            actual researcher.
 	 */
-	public ResearcherAddBackingBean(final ResearcherBean researcherBean, final Session session)
+	public ResearcherAddBackingBean(final ResearcherBean researcherBean,
+			final Session session,
+			final Configuration freemarkerConfiguration)
 	{
 		this.researcherBean = researcherBean;
 		this.session = session;
+		this.freemarkerConfiguration = freemarkerConfiguration;
 	}
 
 	// =================================================================================================================
@@ -81,20 +99,33 @@ public final class ResearcherAddBackingBean
 
 		try
 		{
+			Template emailTemplate = freemarkerConfiguration
+					.getTemplate("org/raziskovalec/messages/emails/informationTemplate.ftl",
+							FacesContext.getCurrentInstance().getViewRoot().getLocale());
+			Map<String, Object> freemarkerContext = Maps.newHashMap();
+			freemarkerContext.put("researcher", researcherBean);
+
+			StringWriter out = new StringWriter();
+			emailTemplate.process(freemarkerContext, out);
 
 			Message message = new MimeMessage(session);
+
+			MimeMultipart multipart = new MimeMultipart();
+
+			BodyPart body = new MimeBodyPart();
+			body.setContent(out.toString(), "text/html");
+
+			multipart.addBodyPart(body);
+
 			message.setFrom(new InternetAddress("Rene Svetina<rene.svetina@gmail.com>"));
 			message.setRecipients(Message.RecipientType.TO,
 					InternetAddress.parse("rene.svetina@gmail.com"));
 			message.setSubject("Raziskovalec mail");
-			message.setText(String.format("Prijavljeni ste v bazo raziskovalce: \nIme in priimek: %s %s\nEmail: %s",
-					researcherBean.getName(),
-					researcherBean.getLastname(),
-					researcherBean.getEmail()));
+			message.setContent(multipart);
 
 			Transport.send(message);
 
-		} catch (MessagingException e)
+		} catch (IOException | MessagingException | TemplateException e)
 		{
 			throw new RuntimeException(e);
 		}
---
+++ raziskovalec-web/src/main/resources/org/raziskovalec/messages/emails/informationTemplate.ftl
@@ -0,0 +1,24 @@
+<html>
+	<body>
+		<div id="container">
+			<div id="header">
+				<h1 style="margin-bottom:0;">Obvestilo - raziskovalec.org</h1>
+			</div>
+			<div id="content">
+				<table>
+					<tr>
+						<td>Ime in priimek: </td>
+						<td>${researcher.name} ${researcher.lastname}</td>
+					</tr>
+					<tr>
+						<td>Email: </td>
+						<td>${researcher.email}</td>
+					</tr>
+				</table>
+			</div>
+			<div id="footer">
+				<p>Copyright © 2012 raziskovalec.org</p>
+			</div>
+		</div>
+	</body>
+</html> 
