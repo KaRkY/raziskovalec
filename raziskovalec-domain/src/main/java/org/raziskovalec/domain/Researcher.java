2942569416d27ef2775ee8aed8c9d4920c20b60f
Rene Svetina <rene.svetina@gmail.com>
2012-06-07 09:21:47 +0200

    Dodal teste

--- raziskovalec-domain/src/main/java/org/raziskovalec/domain/Researcher.java
+++ raziskovalec-domain/src/main/java/org/raziskovalec/domain/Researcher.java
@@ -29,136 +29,89 @@ import com.google.common.base.Objects;
  * 
  * @author Rene Svetina
  */
-public class Researcher implements
-		Serializable
-{
-	// =================================================================================================================
-	// Fields
-	// =================================================================================================================
+public class Researcher implements Serializable {
+    // =================================================================================================================
+    // Fields
+    // =================================================================================================================
+    private static final long serialVersionUID = 5018104292542842256L;
+    private final Identifier id;
+    private Name name;
+    private Name lastName;
+    private InternetAddress email;
 
-	private static final long	serialVersionUID	= 5018104292542842256L;
-	private final Identifier	id;
-	private Name				name;
-	private Name				lastName;
-	private InternetAddress		email;
+    // =================================================================================================================
+    // Constructors
+    // =================================================================================================================
 
-	// =================================================================================================================
-	// Constructors
-	// =================================================================================================================
+    /**
+     * Initializes researcher.
+     * 
+     * @param id
+     *            unique id
+     * @param name
+     *            Personal name
+     * @param lastName
+     *            lastName
+     */
+    public Researcher(final Identifier id, final Name name,
+	    final Name lastName, final InternetAddress email) {
+	this.id = id;
+	this.name = name;
+	this.lastName = lastName;
+	this.email = email;
+    }
 
-	/**
-	 * Default constructor.
-	 */
-	public Researcher()
-	{
-		id = Identifier.newId();
+    // =================================================================================================================
+    // Methods
+    // =================================================================================================================
+
+    /**
+     * @return the name
+     */
+    public Name getName() {
+	return name;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public Name getLastName() {
+	return lastName;
+    }
+
+    /**
+     * @return the email
+     */
+    public InternetAddress getEmail() {
+	return email;
+    }
+
+    /**
+     * @return the id
+     */
+    public Identifier getId() {
+	return id;
+    }
+
+    @Override
+    public int hashCode() {
+	return Objects.hashCode(id);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+	if (obj instanceof Researcher) {
+	    Researcher other = (Researcher) obj;
+
+	    return Objects.equal(id, other.id);
+	} else {
+	    return false;
 	}
+    }
 
-	/**
-	 * Initializes researcher.
-	 * 
-	 * @param id
-	 *            unique id
-	 * @param name
-	 *            Personal name
-	 * @param lastName
-	 *            lastName
-	 */
-	public Researcher(final Identifier id, final Name name, final Name lastName)
-	{
-		this.id = id;
-		this.name = name;
-		this.lastName = lastName;
-	}
-
-	// =================================================================================================================
-	// Methods
-	// =================================================================================================================
-
-	/**
-	 * @return the name
-	 */
-	public Name getName()
-	{
-		return name;
-	}
-
-	/**
-	 * @param name
-	 *            the name to set
-	 */
-	public void setName(final Name name)
-	{
-		this.name = name;
-	}
-
-	/**
-	 * @return the lastName
-	 */
-	public Name getLastName()
-	{
-		return lastName;
-	}
-
-	/**
-	 * @param lastName
-	 *            the lastName to set
-	 */
-	public void setLastName(final Name lastName)
-	{
-		this.lastName = lastName;
-	}
-
-	/**
-	 * @return the email
-	 */
-	public InternetAddress getEmail()
-	{
-		return email;
-	}
-
-	/**
-	 * @param email
-	 *            the email to set
-	 */
-	public void setEmail(final InternetAddress email)
-	{
-		this.email = email;
-	}
-
-	/**
-	 * @return the id
-	 */
-	public Identifier getId()
-	{
-		return id;
-	}
-
-	@Override
-	public int hashCode()
-	{
-		return Objects.hashCode(id);
-	}
-
-	@Override
-	public boolean equals(final Object obj)
-	{
-		if (obj instanceof Researcher)
-		{
-			Researcher other = (Researcher) obj;
-
-			return Objects.equal(id, other.id);
-		}
-		else
-		{
-			return false;
-		}
-	}
-
-	@Override
-	public String toString()
-	{
-		return Objects.toStringHelper(this).add("id", id).add("name", name).add("lastName", lastName)
-				.add("email", email).toString();
-	}
+    @Override
+    public String toString() {
+	return Objects.toStringHelper(this).add("id", id).add("name", name)
+		.add("lastName", lastName).add("email", email).toString();
+    }
 }
---
+++ raziskovalec-domain/src/test/java/org/raziskovalec/domain/OrganizationTest.java
@@ -0,0 +1,29 @@
+package org.raziskovalec.domain;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+import org.raziskovalec.domain.value.Name;
+import org.testng.annotations.Test;
+
+public class OrganizationTest {
+
+    @Test
+    public void createOrganizationWithNameAndEmail() throws AddressException {
+	Organization organization = new Organization(Name.valueOf("FERI"),
+		InternetAddress.parse("feri@uni-mb.si", true)[0]);
+
+	assertThat("Organization:", organization, is(notNullValue()));
+
+	assertThat("Organization name:", organization.name(),
+		is(equalTo(Name.valueOf("FERI"))));
+
+	assertThat("Organization email:", organization.email(),
+		is(equalTo(InternetAddress.parse("feri@uni-mb.si")[0])));
+    }
+}
