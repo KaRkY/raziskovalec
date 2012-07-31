7f83ec2ae15035c9235ab27158a4b57b2f425654
Rene Svetina <rene.svetina@gmail.com>
2012-07-31 15:11:55 +0200

    Šel na starejši release da lahko delam dalje.

--- raziskovalec-web/src/main/java/org/raziskovalec/web/TilesAttributeRequestContextPreparer.java
+++ raziskovalec-web/src/main/java/org/raziskovalec/web/TilesAttributeRequestContextPreparer.java
@@ -9,9 +9,15 @@ public class TilesAttributeRequestContextPreparer implements ViewPreparer {
     @Override
     public void execute(final TilesRequestContext tilesContext,
 	    final AttributeContext attributeContext) {
-	for (final String name : attributeContext.getLocalAttributeNames())
+	/*for (final String name : attributeContext.getLocalAttributeNames())
+	{
+	    System.out.println(name);
 	    tilesContext.getRequestScope().put(name,
 		    attributeContext.getLocalAttribute(name));
+	}*/
+	
+	tilesContext.getRequestScope().put("title",
+		    attributeContext.getLocalAttribute("title"));
     }
 
 }
